0d976e7c43b3372bcf898ed7971183a8fbb94dd7
Soaurabh Kakkar <soaurabh.kakkar@monotype.com>
2016-07-18 12:50:29 +0530

    Initial Commit

---
+++ USK-Animations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  USK-Animations
+//
+//  Created by Soaurabh Kakkar on 18/07/16.
+//  Copyright © 2016 Soaurabh Kakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
